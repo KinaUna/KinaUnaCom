df56fda6b03cd4647578aa21be4d2bd9271cd48b
Per Rosing Mogensen <per.mogensen@live.com>
2019-03-21 09:30:11 +0100

    Security update, added checks to verify if user has rights to view, add or modify content for locations API.

--- KinaUnaProgenyApi/Controllers/LocationsController.cs
+++ KinaUnaProgenyApi/Controllers/LocationsController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KinaUna.Data;
 using KinaUna.Data.Contexts;
+using KinaUna.Data.Extensions;
 using KinaUna.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,30 +24,27 @@ namespace KinaUnaProgenyApi.Controllers
             _context = context;
 
         }
-        // GET api/locations
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            List<Location> resultList = await _context.LocationsDb.AsNoTracking().ToListAsync();
 
-            return Ok(resultList);
-        }
 
         // GET api/locations/progeny/[id]
         [HttpGet]
         [Route("[action]/{id}")]
         public async Task<IActionResult> Progeny(int id, [FromQuery] int accessLevel = 5)
         {
-            List<Location> locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == id && l.AccessLevel >= accessLevel).ToListAsync();
-            if (locationsList.Any())
+            string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+            UserAccess userAccess = _context.UserAccessDb.AsNoTracking().SingleOrDefault(u =>
+                u.ProgenyId == id && u.UserId.ToUpper() == userEmail.ToUpper());
+            if (userAccess != null || id == Constants.DefaultChildId)
             {
-                return Ok(locationsList);
-            }
-            else
-            {
+                List<Location> locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == id && l.AccessLevel >= accessLevel).ToListAsync();
+                if (locationsList.Any())
+                {
+                    return Ok(locationsList);
+                }
                 return NotFound();
             }
 
+            return Unauthorized();
         }
 
         // GET api/locations/5
@@ -54,13 +53,43 @@ namespace KinaUnaProgenyApi.Controllers
         {
             Location result = await _context.LocationsDb.AsNoTracking().SingleOrDefaultAsync(l => l.LocationId == id);
 
-            return Ok(result);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+            UserAccess userAccess = _context.UserAccessDb.AsNoTracking().SingleOrDefault(u =>
+                u.ProgenyId == result.ProgenyId && u.UserId.ToUpper() == userEmail.ToUpper());
+            if (userAccess != null || id == Constants.DefaultChildId)
+            {
+                return Ok(result);
+            }
+
+            return Unauthorized();
         }
 
         // POST api/timeline
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Location value)
         {
+            // Check if child exists.
+            Progeny prog = await _context.ProgenyDb.AsNoTracking().SingleOrDefaultAsync(p => p.Id == value.ProgenyId);
+            string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+            if (prog != null)
+            {
+                // Check if user is allowed to add locations for this child.
+
+                if (!prog.Admins.ToUpper().Contains(userEmail.ToUpper()))
+                {
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+
             Location location = new Location();
             location.AccessLevel = value.AccessLevel;
             location.Author = value.Author;
@@ -80,7 +109,7 @@ namespace KinaUnaProgenyApi.Controllers
             location.State = value.State;
             location.StreetName = value.StreetName;
             location.Tags = value.Tags;
-            
+
             _context.LocationsDb.Add(location);
             await _context.SaveChangesAsync();
 
@@ -91,6 +120,22 @@ namespace KinaUnaProgenyApi.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> Put(int id, [FromBody] Location value)
         {
+            // Check if child exists.
+            Progeny prog = await _context.ProgenyDb.AsNoTracking().SingleOrDefaultAsync(p => p.Id == value.ProgenyId);
+            string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+            if (prog != null)
+            {
+                // Check if user is allowed to add locations for this child.
+                if (!prog.Admins.ToUpper().Contains(userEmail.ToUpper()))
+                {
+                    return Unauthorized();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+
             Location location = await _context.LocationsDb.SingleOrDefaultAsync(l => l.LocationId == id);
             if (location == null)
             {
@@ -129,14 +174,28 @@ namespace KinaUnaProgenyApi.Controllers
             Location location = await _context.LocationsDb.SingleOrDefaultAsync(l => l.LocationId == id);
             if (location != null)
             {
+                // Check if child exists.
+                Progeny prog = await _context.ProgenyDb.AsNoTracking().SingleOrDefaultAsync(p => p.Id == location.ProgenyId);
+                string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+                if (prog != null)
+                {
+                    // Check if user is allowed to delete locations for this child.
+                    if (!prog.Admins.ToUpper().Contains(userEmail.ToUpper()))
+                    {
+                        return Unauthorized();
+                    }
+                }
+                else
+                {
+                    return NotFound();
+                }
+
                 _context.LocationsDb.Remove(location);
                 await _context.SaveChangesAsync();
                 return NoContent();
             }
-            else
-            {
-                return NotFound();
-            }
+
+            return NotFound();
         }
 
 
@@ -144,8 +203,21 @@ namespace KinaUnaProgenyApi.Controllers
         public async Task<IActionResult> GetLocationMobile(int id)
         {
             Location result = await _context.LocationsDb.AsNoTracking().SingleOrDefaultAsync(l => l.LocationId == id);
+            if (result != null)
+            {
+                string userEmail = User.GetEmail() ?? Constants.DefaultUserEmail;
+                UserAccess userAccess = _context.UserAccessDb.AsNoTracking().SingleOrDefault(u =>
+                    u.ProgenyId == result.ProgenyId && u.UserId.ToUpper() == userEmail.ToUpper());
 
-            return Ok(result);
+                if (userAccess != null || result.ProgenyId == Constants.DefaultChildId)
+                {
+                    return Ok(result);
+                }
+
+                return Unauthorized();
+            }
+
+            return NotFound();
         }
     }
 }
