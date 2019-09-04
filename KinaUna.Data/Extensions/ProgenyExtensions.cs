8783a5be217cb57e73781f3596aceb771ae55aff
Per Rosing Mogensen <per.mogensen@live.com>
2019-09-04 10:41:54 +0200

    Added ProgenyExtensions to add a function to check if a given string (email) is in the Progeny's admin list.

---
+++ KinaUna.Data/Extensions/ProgenyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KinaUna.Data.Models;
+
+namespace KinaUna.Data.Extensions
+{
+    public static class ProgenyExtensions
+    {
+        /// <summary>
+        /// Verifies if an email address is in the admin list.
+        /// </summary>
+        /// <param name="email">string: The email address to verify.</param>
+        /// <returns>bool: True if the email address is in the admin list, otherwise false.</returns>
+        public static bool IsInAdminList(this Progeny progeny, string email)
+        {
+            string[] adminList = progeny.Admins.Split(',');
+            progeny.Admins = "";
+            foreach (string adminItem in adminList)
+            {
+                if (!adminItem.Trim().ToUpper().Equals(email.Trim().ToUpper()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
